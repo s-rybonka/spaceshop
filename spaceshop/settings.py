ede76b579b51f747a22ddb8d931ea20103d95ab6
Stanislav-Rybonka <stanislav.rybonka@gmail.com>
2017-05-16 20:07:34 +0300

    set DEBUG to False

--- spaceshop/settings.py
+++ spaceshop/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'stf)=8djmvems$$lnmc-2@o9puwf&$u(ui51nvdy2vs$)%d$v+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
