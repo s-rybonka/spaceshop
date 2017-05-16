f407c5dbcc7759b780bebfde01cb9e7cd7629a0a
Stanislav-Rybonka <stanislav.rybonka@gmail.com>
2017-05-16 19:14:38 +0300

    fix setings

--- spaceshop/settings.py
+++ spaceshop/settings.py
@@ -13,12 +13,12 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 from django.core.urlresolvers import reverse_lazy
 import psycopg2
-import urlparse3
-import urllib
+import urlparse3.urlparse3
+from urllib.parse import urlparse
 
 
-urlparse3.uses_netloc.append("postgres")
-url = urlparse3.parse_url(os.environ["DATABASE_URL"])
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
 conn = psycopg2.connect(
     database=url.path[1:],
